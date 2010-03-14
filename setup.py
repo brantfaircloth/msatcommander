6ebadc80f6870308c93b5f96f3f0ffb3091c702c
Brant Faircloth <brant.faircloth@gmail.com>
2010-03-14 13:28:04 -0700

    Editing setup again to alter for py2app and my py2app hack

--- setup.py
+++ setup.py
@@ -8,8 +8,23 @@ Usage:
 from setuptools import setup
 import os, sys, shutil
 
-# add sys.path = [os.path.join(os.environ['RESOURCEPATH'], 'lib', 'python2.6', 'lib-dynload')] + sys.path
-# to dist/msatcommander.app/Contents/Resources/__boot__.py
+
+# in order to add sys.path = [os.path.join(os.environ['RESOURCEPATH'], 'lib', 'python2.6', 'lib-dynload')] + sys.path
+# to dist/msatcommander.app/Contents/Resources/__boot__.py, i hacked
+# /Library/Python/2.6/site-packages/py2app-0.4.4-py2.6.egg/py2app @ line 1996
+# to:
+#
+# data = self.get_bootstrap_data(fn)
+# if os.path.split(fn)[-1] == 'boot_app.py':
+#     data = data.split('\n')
+#     temp = data[:3]
+#     ver = sys.version_info[:2]
+#     ver = 'python%s.%s' % (ver[0], ver[1])
+#     pathinfo = '''    sys.path = [os.path.join(os.environ['RESOURCEPATH'], 'lib', '%s', 'lib-dynload')] + sys.path''' % ver
+#     temp.append(pathinfo)
+#     temp += data[3:]
+#     data = '\n'.join(temp)
+# bootfile.write(data)
 
 # remove build directories
 print 'Deleting build dirs...'
@@ -27,11 +42,30 @@ if os.name == 'posix':
         app=['main.py'],
         setup_requires=["py2app"],
         options=dict(py2app=dict(
-                includes=['Bio.SeqIO', 'p3wrapr', 'PyQt4', 'PyQt4.QtCore', 'PyQt4.QtGui', 'sip'],
+                includes=['Bio.SeqIO',
+                            'p3wrapr',
+                            'PyQt4',
+                            'PyQt4.QtCore',
+                            'PyQt4.QtGui',
+                            'sip'],
                 packages=[],
                 resources=['primer3_core',
-                'misprime_lib_weight'],
-                excludes=['p3wrapr/.git', 'Bio.nexus', 'Scipy', 'numpy', 'PyQt4.QtDesigner', 'PyQt4.QtNetwork', 'PyQt4.QtOpenGL', 'PyQt4.QtScript', 'PyQt4.QtSql', 'PyQt4.QtTest', 'PyQt4.QtWebKit', 'PyQt4.QtXml', 'PyQt4.phonon']
+                            'misprime_lib_weight',
+                            'primer3_config'],
+                excludes=['/Users/bcf/git/brant/modules/p3wrapr/docs/',
+                            '/Users/bcf/git/brant/modules/p3wrapr/.git',
+                            'Bio.nexus',
+                            'Scipy',
+                            'numpy',
+                            'PyQt4.QtDesigner',
+                            'PyQt4.QtNetwork',
+                            'PyQt4.QtOpenGL',
+                            'PyQt4.QtScript',
+                            'PyQt4.QtSql',
+                            'PyQt4.QtTest',
+                            'PyQt4.QtWebKit',
+                            'PyQt4.QtXml',
+                            'PyQt4.phonon']
             ))
     )
     
